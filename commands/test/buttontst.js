b1e303d216d06bbfc8f89a7b85f7c4f3e0320ad6
DrWhilson <DrWhilsonMFI@gmail.com>
2023-12-27 00:28:53 +0700

    select

--- commands/test/buttontst.js
+++ commands/test/buttontst.js
@@ -1,28 +1,58 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require('discord.js');
+const { ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('buttontst')
     .setDescription('Create a button'),
   async execute(interaction) {
-    const target = interaction.options.getUser('target');
-    const reason = interaction.options.getString('reason') ?? 'No reason provided';
+    const target = interaction.options.getUser('target') ?? 'No User';
 
-    const confirm = new ButtonBuilder()
-      .setCustomId('confirm')
-      .setLabel('Confirm Ban')
+    const BtnConfim = new ButtonBuilder()
+      .setCustomId('confim')
+      .setLabel('Подтвердить ')
+      .setStyle(ButtonStyle.Primary);
+
+    const BtnCancel = new ButtonBuilder()
+      .setCustomId('cancel')
+      .setLabel('Отмена')
       .setStyle(ButtonStyle.Danger);
 
-    const cancel = new ButtonBuilder()
-      .setCustomId('cancel')
-      .setLabel('Cancel')
-      .setStyle(ButtonStyle.Secondary);
+    const select = new StringSelectMenuBuilder()
+      .setCustomId('usrconf')
+      .setPlaceholder('Chose interaction')
+      .addOptions(
+        new StringSelectMenuOptionBuilder()
+          .setLabel('Request')
+          .setDescription('Role Change Request')
+          .setValue('rcrequest'),
+        new StringSelectMenuOptionBuilder()
+          .setLabel('Mute')
+          .setDescription('Mute User')
+          .setValue('muteuser'),
+        new StringSelectMenuOptionBuilder()
+          .setLabel('Time Out')
+          .setDescription('Give a Time Out status')
+          .setValue('timeout'),
+        new StringSelectMenuOptionBuilder()
+          .setLabel('Ban')
+          .setDescription('Ban User')
+          .setValue('ban'),
+        new StringSelectMenuOptionBuilder()
+          .setLabel('Change Name')
+          .setDescription('Change User Name')
+          .setValue('changename'),
+        new StringSelectMenuOptionBuilder()
+          .setLabel('Change Role')
+          .setDescription('Change User Role')
+          .setValue('changerloe'),
+
+      );
 
     const row = new ActionRowBuilder()
-      .addComponents(cancel, confirm);
+      .addComponents(select);
 
     await interaction.reply({
-      content: `Are you sure you want to ban ${target} for reason: ${reason}?`,
+      content: `${target}`,
       components: [row],
     });
   },
